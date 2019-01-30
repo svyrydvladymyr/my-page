993e2cba368fd6c1242d1faeed2773a679393b59
Svyryd Vladymyr <svyrydvladimir@gmail.com>
2019-01-30 13:45:25 +0200

    start3

---
+++ JavaScript.js
@@ -0,0 +1,6 @@
+var x = angular.module("myApp", []).controller("coNtrol", function($scope){
+    $scope.menu = "MENU";
+    $scope.menu_punkt1 = "main";
+    $scope.menu_punkt2 = "second";
+    $scope.menu_punkt3 = "third";
+}); 
